63c56ad8313827d6c62f1cc16ee02280f8584878
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-09-07 17:07:13 +0200

    chore(deps): migrate to spotify-web-api-java v10

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,7 +11,6 @@ module de.dargmuesli.spotitag {
     requires kotlinx.serialization.core;
     requires kotlinx.serialization.json;
     requires mp3agic;
-    requires nv.i18n;
     requires org.apache.commons.text;
     requires org.apache.logging.log4j;
     requires org.kordamp.ikonli.core;
